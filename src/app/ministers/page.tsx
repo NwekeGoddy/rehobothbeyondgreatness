e1b6d52b77000ef4bcc1793a03ba58c678a415d1
Nweke Chidi <nwekechidigodwin460@gmail.com>
2024-07-06 21:17:26 +0100

    "Remove default Next.js template code and replace with a simple 'Make Sense' text"
    Signed-off-by: Nweke Chidi <nwekechidigodwin460@gmail.com>

---
+++ src/app/ministers/page.tsx
@@ -0,0 +1,21 @@
+
+export default function Page() {
+    return (
+      <div className="flex min-h-screen flex-col items-center justify-between h-[500px]">
+        <div className="w-full h-full">
+          <div className="relative" style={{ paddingBottom: "56.25%" /* 16:9 aspect ratio */ }}>
+            <iframe
+              src="https://docs.google.com/forms/d/e/1FAIpQLScO11otVSO-aEIGbYu2PO1u45aPuPy9rHEnPnzCmCltEF5jeQ/viewform?embedded=true"
+              className="absolute top-0 left-0 w-full min-h-screen"
+              style={{ border: 0 }}
+              allowFullScreen
+              title="Google Form"
+            >
+              Loading…
+            </iframe>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
