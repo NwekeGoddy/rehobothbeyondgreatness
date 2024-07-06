0698118fded477047a2d3c04ba7ec0f389f3106c
Nweke Chidi <nwekechidigodwin460@gmail.com>
2024-07-06 21:26:44 +0100

    "Update text content in Home component from 'Make Sense' to 'Rehoboth Beyond Greatness Website'"
    Signed-off-by: Nweke Chidi <nwekechidigodwin460@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between h-[500px]">
-      Make Sense
+     Rehoboth Beyond Greatness Website
     </main>
   );
 }
